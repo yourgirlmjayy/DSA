50f34c2fefe6ea20826869f4372df5344a0e6105
Mojolajesu Dada <dadamojola4@gmail.com>
2024-08-29 11:48:43 -0500

    implemented methods for array class

--- OneDimensionalArray/onedimensionalarray.java
+++ OneDimensionalArray/onedimensionalarray.java
@@ -24,6 +24,59 @@ public class OneDimensionalArray {
     arr = oneArray;
   }
 
+  //methods
+  // get an element by index
+  public int getElement(int index){
+    return arr[index];
+  }
+  // set an element by index
+  public void setElement(int index, int value){
+    arr[index] = value;
+  }
+  // add an element
+  public void addElement(int value){
+    arr[size] = value;
+    size++;
+  }
+  // get size
+  public int getSize(){return size;}
+  
+  // Traverse
+  public void traverse(){
+    for(int i=0; i<size; i++){
+      System.out.print(arr[i]);
+      System.out.print(" ");
+    }
+    System.out.println();
+  }
+  // Insertion
+  public void insert(int index, int value) throws IndexOutOfBoundsException{
+    // case 1: invaid index
+    if(index>size || index<0){
+      throw new IndexOutOfBoundsException("Invalid index: " + index);
+    }
+    // case 2 valid index, but size<length
+    if(size<length){
+      for(int i=size; i>index; i--){
+        arr[i] = arr[i-1];
+      }
+      arr[index] = value;
+    }else{ // case 3: valid index, but size=length
+      int[] newArr = new int[length*2];
+      length *= 2;
+      for(int i=0; i<index; i++){
+        newArr[i] = arr[i];
+      }
+      newArr[index] = value;
+      for(int i=index+1; i<size+1; i++){
+        newArr[i] = arr[i-1];
+      }
+      arr = newArr;
+    }
+    
+    size++;
+  }
+  
 
 
 
@@ -35,6 +88,10 @@ public class OneDimensionalArray {
 
 
 
+
+public static void main (String [] args) {
+  System.out.println('2');
+}
 
 
 
